8539576c976468cd75a5b0f4cc8cdcaec9b81566
krzysztof113-commits <krzysztof113@hotmail.com>
2020-08-22 14:27:17 +0200

    Chapter 4 - Preparing the database model

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from config import Config
+# by installing extensions (I see they are actually wrappers in some cases) they can also install incompatible by
+# default components like SQLAlchemy, the same name they have inside extension so instead of importing them directly
+# we are importing them by extensions
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # __name__ is something like a mode of flask application that is default and works good on basic projects
 app = Flask(__name__)
 # importing flask settings from specific object (class) to keep config.py and __init__.py separate files
 app.config.from_object(Config)
+# we also need instances for database and migrate tool for it since it works based on instances
+# instance of something is a place in the memory and software so it can work along an application
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # routes will need the app variable, to prevent looping (circular imports) we put it after the app variable (object)
-from app import routes
+# models will be used to define the structure of the database
+from app import routes, models
--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
 
+# abspath is a function that gives like that in default be e.g. /templates or /
+# we need a full path so as an argument needs to be name of our project directory, we can use the magic function
+# We are using os.path instead of manually typing or most important joining things because there can be different
+# paths for windows, linux, macos etc.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
 	# An environment variable is a variable whose value is set outside the program,
 	# typically through functionality built into the operating system or microservice.
 	# Here is cryptographic key that is used by many applications and flask extensions (e.g. tokens or signatures).
 	# WTForms uses it to encrypt against Cross-Site Request Forgery (CSRF) attacks. Only admins know the key.
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	# configs for databases - SQLAlchemy
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	# this one makes some flask errors or maybe additional steps, warnings if changing databases so we turn it off
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
