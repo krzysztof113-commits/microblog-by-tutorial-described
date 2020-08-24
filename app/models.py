6084573a2d8976851ff9526140da81981a0cf048
krzysztof113-commits <krzysztof113@hotmail.com>
2020-08-24 17:13:14 +0200

    Chapter 4 - Database Relationships

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 
@@ -6,7 +7,22 @@ class User(db.Model):
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
+	# posts is what is necessary to display user of post which has some relationship with it, it's gonna be post.author
+	# (for post in posts: print(post.author)), backref is a name of object that's gonna be created, not a Column
+	posts = db.relationship('Post', backref='author', lazy='dynamic')
 
 	# the way how the record of the table (object) is printed
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	body = db.Column(db.String(140))
+	# adding function name rather than its instance (datetime.utc now()) so it can be user many times
+	# indexing it to search, organise etc. easier, faster
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Post {}>'.format(self.body)
