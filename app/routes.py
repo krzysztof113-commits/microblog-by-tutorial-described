4d7a1cb3ea46c374304c19d48d6ea9ea98b5ba64
krzysztof113-commits <krzysztof113@hotmail.com>
2020-09-10 20:05:24 +0200

    Chapter 9 - Page navigation

--- app/routes.py
+++ app/routes.py
@@ -36,9 +36,12 @@ def index():
 		return redirect(url_for('index'))
 	page = request.args.get('page', 1, type=int)
 	posts = current_user.followed_posts().paginate(
-		# False here is to not display errors if there are not enought posts to show but empty lists
+		# False here is to not display errors if there are not enough posts to show but empty lists
 		page, app.config['POSTS_PER_PAGE'], False)
-	return render_template('index.html', title='Home Page', form=form, posts=posts.items)
+	next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+	prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+	return render_template('index.html', title='Home Page', form=form, posts=posts.items,
+						   next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/explore')
@@ -47,7 +50,10 @@ def explore():
 	page = request.args.get('page', 1, type=int)
 	posts = Post.query.order_by(Post.timestamp.desc()).paginate(
 		page, app.config['POSTS_PER_PAGE'], False)
-	return render_template('index.html', title='Explore', posts=posts.items)
+	next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+	prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+	return render_template('index.html', title='Explore', posts=posts.items,
+						   next_url=next_url, prev_url=prev_url)
 
 
 # default is GET method but we want more of it because GET is mainly used for getting things from server
@@ -97,12 +103,16 @@ def register():
 @login_required
 def user(username):
 	user = User.query.filter_by(username=username).first_or_404()
-	posts = [
-		{'author': user, 'body': 'Test post #1'},
-		{'author': user, 'body': 'Test post #2'}
-	]
+	page = request.args.get('page', 1, type=int)
+	# I take advantage of the fact that the user.posts relationship is a query that is already set up by SQLAlchemy
+	# as a result of the db.relationship() definition in the User model. I take this query and add a order_by()...
+	posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+		page, app.config['POSTS_PER_PAGE'], False)
+	next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+	prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
 	form = EmptyForm()
-	return render_template('user.html', user=user, posts=posts, form=form)
+	return render_template('user.html', user=user, posts=posts.items,
+						   next_url=next_url, prev_url=prev_url, form=form)
 
 
 @app.before_request
