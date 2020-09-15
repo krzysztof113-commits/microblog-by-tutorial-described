1ef37e8ae21b7c67ea174708b0f8bf5f178d7cf5
krzysztof113-commits <krzysztof113@hotmail.com>
2020-09-15 17:50:10 +0200

    Chapter 10 - Email async and threads

--- app/email.py
+++ app/email.py
@@ -2,13 +2,23 @@ from flask_mail import Message
 from app import mail
 from flask import render_template
 from app import app
+from threading import Thread
+
+
+def send_async_email(app, msg):
+	# Flask uses contexts to avoid having to pass arguments across functions
+	# Thread needs application instance in case to work, in overall it's not necessary but in case of custom threads
+	with app.app_context():
+		mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
 	msg = Message(subject, sender=sender, recipients=recipients)
 	msg.body = text_body
 	msg.html = html_body
-	mail.send(msg)
+	# With this change, the sending of the email will run in the thread,
+	# and when the process completes the thread will end and clean itself up.
+	Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user):
